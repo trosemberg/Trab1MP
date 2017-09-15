dd3f1ad4a616c06064c3416c2e23874450d95160
trosemberg <tomasroro@hotmail.com>
2017-09-15 17:55:23 -0300

    testando soma com separador certo posição errada funcionando

--- src/string_soma.cpp
+++ src/string_soma.cpp
@@ -8,10 +8,14 @@ const static int ajustenum = 48;//constante para pegar o numero na tabela ascci 
 
 
 int soma_string(char * string_entrada ){
-	int tamstr = 0,soma = 0,ndigitos = 0,nnum = 0;
+	int tamstr = 0,soma = 0,ndigitos = 0,nnum = 0,sep=1;
 	//tamstr é uma variavel que armazena o tamanho da string de entrada, desta forma 
 	//ndigitos é uma variavel usada para identificar se o numero passado esta na casa das unidades, dezenas ou centenas.
 	//nnum é um contador para o numero de numeros digitados
+	/*a varieavel sep, server pra contar o separador,se encontrar um separador ela é setada para 1, se encontrar um numero ela é setada para 0, seta forma é possivel 
+	saber se esta encontrando dois separadores sem ter numero entre eles,ou se ainda nao apareceu nenhum numero antes do primeiro separador,por isso começamos
+	a, o que deve acarretar numa entrada invalida*/
+
 	tamstr = strlen(string_entrada);
 	if(tamstr < 2){
 		soma = -1;
@@ -21,13 +25,17 @@ int soma_string(char * string_entrada ){
 		return soma;
 	}
 	for (int i = 0; i < tamstr; i++){
-
 		if((string_entrada[i]>='0') && (string_entrada [i]<='9')){
+			sep=0;
 			ndigitos++;
 			if(ndigitos == 1){
 				nnum++;
 			}
-		}else if ((string_entrada[i]!=',') && ((string_entrada[i]!=cbarra) && (string_entrada[i+1]!='n')) && ((string_entrada[i-1]!=cbarra) && (string_entrada[i]!='n'))){
+		}else if ((string_entrada[i]!=',') && ((string_entrada[i]!=cbarra) && (string_entrada[i+1]!='n')) && ((string_entrada[i]!='n') && (string_entrada[i-1]!=cbarra))){
+			return -1;
+		}else if (string_entrada[0]==','){
+				return -1;
+		}else if(string_entrada[i]==',' && sep == 1){
 			return -1;
 		}else{
 			if(ndigitos<4){
@@ -37,13 +45,10 @@ int soma_string(char * string_entrada ){
 			}
 			ndigitos = 0;
 		}
-
+		if(string_entrada[i]==','){
+			sep = 1;
+		}
 	}
-
-
-
-
-
 
 	return soma;
 }
