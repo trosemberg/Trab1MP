4b8ebe7ebd865f539ae4c4634b9787abdac3b28b
trosemberg <tomasroro@hotmail.com>
2017-09-15 02:11:44 -0300

    primeiro teste feito

--- src/testa_string_soma.cpp
+++ src/testa_string_soma.cpp
@@ -2,9 +2,14 @@
 #include <gtest/gtest.h>
 #include <iostream>
 using namespace std;
-char oi ='o';
-TEST(soma_string, testebasico) { 
-    ASSERT_EQ(1, soma_string(&oi));
+
+
+
+
+TEST(soma_string, primeiroteste) { 
+	char teste1[100];
+	cin>> teste1;
+    ASSERT_EQ(1, soma_string(teste1));
 }
 
 int main(int argc, char **argv) {
