cbd7bf81fed589785cc964bf1065f8d80a668d33
trosemberg <tomasroro@hotmail.com>
2017-09-15 10:22:16 -0300

    parando de usar alocação dinamica no teste

---
+++ src/string_soma.hpp
@@ -0,0 +1,12 @@
+#ifndef STRING_SOMA_HPP
+#define STRING_SOMA_HPP
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+
+int soma_string(char * string_entrada );
+
+#endif
