e87e64197d9312138779ef6cce16b8ffd18e90c1
trosemberg <tomasroro@hotmail.com>
2017-09-15 02:56:15 -0300

    primeiro teste usando \n funcionando

--- src/string_soma.cpp
+++ src/string_soma.cpp
@@ -4,7 +4,12 @@ using namespace std;
 
 
 int soma_string(char * string_entrada ){
-	int tamanhostring,soma = 0, i = 0;
-	tamanhostring = strlen(string_entrada);
+	int tamstr,soma = 0, i = 0;
+	tamstr = strlen(string_entrada);
+	if(tamstr < 2){
+		soma = -1;
+	}else if((string_entrada[tamstr-1]!= 'n') && (string_entrada[tamstr-2])){
+		soma = -1;
+	}
 	return soma;
 }
