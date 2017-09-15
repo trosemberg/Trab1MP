6f844123290a5ecc0e5793c73db700297252ef1c
trosemberg <tomasroro@hotmail.com>
2017-09-15 02:49:18 -0300

    primeiro teste usando \n

--- src/testa_string_soma.cpp
+++ src/testa_string_soma.cpp
@@ -7,14 +7,11 @@ using namespace std;
 
 
 TEST(soma_string, primeiroteste) { 
-	char *str1,*str2;
-	str1 = (char *) malloc (strlen("ola"));
-	strcpy(str1,"ola");
-	str2 = (char *) malloc (strlen("\n"));
+	char *str1;
+	str1 = (char *) malloc (strlen("\n"));
 	strcpy(str1,"\n");
     
     ASSERT_EQ(-1, soma_string(str1));
-    ASSERT_EQ(-1, soma_string(str2));
 
 }
 
