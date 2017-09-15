61b8bb8d042eb4a1cca095344bffce60766d7807
trosemberg <tomasroro@hotmail.com>
2017-09-15 11:21:45 -0300

    testando soma sem separador

--- src/testa_string_soma.cpp
+++ src/testa_string_soma.cpp
@@ -10,7 +10,7 @@ TEST(soma_string, primeiroteste) {
 	char str1[100];
 	strcpy(str1,"\n");
     
-    ASSERT_EQ(-1, soma_string(str1));
+    ASSERT_EQ(0, soma_string(str1));
 
 }
 
@@ -20,10 +20,20 @@ TEST(soma_string, string_pequena_ou_sem_barran) {
     strcpy(str2,"");
     strcpy(str3,"1233");
     ASSERT_EQ(-1, soma_string(str1));
-    ASSERT_EQ(-1, soma_string(str1));
-    ASSERT_EQ(-1, soma_string(str1));
+    ASSERT_EQ(-1, soma_string(str2));
+    ASSERT_EQ(-1, soma_string(str3));
 
 }
+
+TEST(soma_string,testando_soma){
+	char str1[10],str2[10];
+	strcpy(str1,"\n");
+    strcpy(str2,"1\n");
+    ASSERT_EQ(0,soma_string(str1));
+    ASSERT_EQ(1,soma_string(str2));
+}
+
+
 
 
 int main(int argc, char **argv) {
