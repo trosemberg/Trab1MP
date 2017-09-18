596ae1657f83e81056e387f9c810d79d8a3d0223
trosemberg <tomasroro@hotmail.com>
2017-09-18 01:56:28 -0300

    atualizando a main

--- src/testa_soma_string_stdin.cpp
+++ src/testa_soma_string_stdin.cpp
@@ -6,9 +6,8 @@ using namespace std;
 int main(){
 int soma;
 char oi[1000];
-cout<< "digite a sua entrada:"<<endl;
 cin>> oi; 
 soma = soma_string(oi);
-cout<< "A soma é igual a: "<< soma <<endl;
+cout<<soma <<endl;
 return 0;
 }
